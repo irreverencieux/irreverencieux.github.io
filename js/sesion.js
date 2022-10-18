ea2bb104b892445f7c1dca26f9c70fb4349d6525
Orlando Gomez <irreverencieux@gmail.com>
2022-10-17 22:48:58 -0500

    Mas arreglos

--- js/sesion.js
+++ js/sesion.js
@@ -1,22 +1,4 @@
-hello.on('auth.login', function (auth) {
 
-        // add a greeting and access the thumbnail and name from
-        // the authorized response
-
-          hello(auth.network).api('/me').then(function (resp) {
-                var lab = document.createElement("div");
-                lab.id = "pic_and_greet";
-                lab.innerHTML = '<img src="' + resp.thumbnail + '" /> Hey ' + resp.name;
-                document.body.appendChild(lab);
-                });
-        });
-
-// remove the greeting when we log out
-
-hello.on('auth.logout', function () {
-  var lab = document.getElementById("pic_and_greet");
-  if (lab != null) document.body.removeChild( lab );
-});
 
 /*
 hello.on('auth.login', function(auth) {
